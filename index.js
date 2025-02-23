c1e18fa6458b5c95cd815fdd3ea8f7fcf6274557
MrPan <92256188+StoreVia@users.noreply.github.com>
2025-02-23 15:50:27 +0530

    1

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const Express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const express = require("express");
+const fs = require("fs");
 
-const app = Express();
-const server = http.createServer(app);
-const io = new Server(server);
+const app = express();
+const dares = JSON.parse(fs.readFileSync("./jsons/dares.json"));
+const truth = JSON.parse(fs.readFileSync("./jsons/truth.json"));
+const fact = JSON.parse(fs.readFileSync("./jsons/fact.json"));
+const names = JSON.parse(fs.readFileSync("./jsons/names.json"));
 
-app.use("/", require("./server"));
+app.get("/dare", (req, res) => res.json({ dare: dares[Math.random() * dares.length | 0] }));
+app.get("/truth", (req, res) => res.json({ truth: truth[Math.random() * truth.length | 0] }));
+app.get("/fact", (req, res) => res.json({ fact: fact[Math.random() * fact.length | 0] }));
+app.get("/name", (req, res) => res.json({ name: names[Math.random() * names.length | 0] }));
 
-server.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000);
