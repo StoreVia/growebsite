4694818f7ccaea7bc9a9bcbe608d6911a4607db5
MrPan <92256188+StoreVia@users.noreply.github.com>
2025-02-23 16:06:11 +0530

    1

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const express = require("express");
-const fs = require("fs");
 const path = require("path");
+const express = require("express");
 
 const app = express();
-const dares = JSON.parse(fs.readFileSync("./jsons/dares.json"));
-const truth = JSON.parse(fs.readFileSync("./jsons/truth.json"));
-const fact = JSON.parse(fs.readFileSync("./jsons/fact.json"));
-const names = JSON.parse(fs.readFileSync("./jsons/names.json"));
 
-app.use(express.static("public"));
+// Serve static files from the public directory
+app.use(express.static(path.join(process.cwd(), "public")));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
-app.get("/dare", (req, res) => res.json({ dare: dares[Math.random() * dares.length | 0] }));
-app.get("/truth", (req, res) => res.json({ truth: truth[Math.random() * truth.length | 0] }));
-app.get("/fact", (req, res) => res.json({ fact: fact[Math.random() * fact.length | 0] }));
-app.get("/name", (req, res) => res.json({ name: names[Math.random() * names.length | 0] }));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(process.cwd(), "public", "index.html"));
+});
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
